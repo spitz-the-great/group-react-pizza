0729072f837e34f4e2e08683a37810aa63dbbf54
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-06 13:57:07 -0500

    set up redux store in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cart = (state = {}, action) => {
+    if (action.type === 'ADD_PIZZAS') {
+        // SelectPage logic here
+
+
+    } else if (action.type === 'ADD_CUSTOMER') {
+        // CustomerPage logic here
+
+
+    } else if (action.type === 'EMPTY_CART') {
+        // CheckoutPage logic here
+
+
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({cart}),
+    applyMiddleware(logger);
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
