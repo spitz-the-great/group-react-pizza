597b161cf558fc00ac0f83793eab5cc1cffcee18
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-06 13:35:53 -0500

    set up routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import SelectPage from '../SelectPage/SelectPage.js';
+import CustomerPage from '../CustomerPage/CustomerPage.js';
+import CheckoutPage from '../CheckoutPage/CheckoutPage.js';
+import AdminPage from '../AdminPage/AdminPage.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Prime Pizza</h1>
-        </header>
-        <br/>
-        <img src="images/pizza_photo.png"/>
-        <p>Pizza is great.</p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Prime Pizza</h1>
+          </header>
+          <Route exact path="/" component={SelectPage} />
+          <Route path="/customer" component={CustomerPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+          <Route path="/admin" component={AdminPage} />
+        </div>
+      </Router>
     );
   }
 }
